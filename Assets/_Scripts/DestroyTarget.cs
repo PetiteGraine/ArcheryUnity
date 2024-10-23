d4197a9a12e8ee7176080d2417fda84789c0575f
Eugenienakouzi <94555318+Eugenienakouzi@users.noreply.github.com>
2024-10-23 19:05:23 +0200

    point supplémentaire centre cible

--- Assets/_Scripts/DestroyTarget.cs
+++ Assets/_Scripts/DestroyTarget.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DestroyTarget : MonoBehaviour
 {
+    public int pointgagne = 10;
     public GameObject target;
     [SerializeField] private GameObject soundEffect;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Arrow"))
         {
             GameObject spawnedEffect = Instantiate(soundEffect);
             spawnedEffect.transform.position = gameObject.transform.position;
-            Manager.instance.UpdateScore(10);
+            Manager.instance.UpdateScore(pointgagne);
             Destroy(collision.gameObject);
             Instantiate(target);
             Destroy(gameObject);
